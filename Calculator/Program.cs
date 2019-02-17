ce2f9e396cc8a95a0aa7a38f2148f4d8322da32e
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-02-17 17:57:26 -0600

    Homework Finished

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,8 +6,31 @@ namespace Calculator
     {
         public static void Main()
         {
+            Console.WriteLine("Pick a number");
+            int numOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Pick another number");
+            int numTwo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Do you want to add, subtract, multiply, or divide the two numbers");
+            string choice = Console.ReadLine();
+            choice = choice.ToLower();
             int result = 0;
-
+            if(choice == "add" || choice == "+")
+            {
+                result = numOne + numTwo;
+            }
+            if(choice == "subtract" || choice == "-")
+            {
+                result = numOne - numTwo;
+            }
+            if(choice == "multiply" || choice == "x" || choice == "*")
+            {
+                result = numOne * numTwo;
+            }
+            if(choice == "divide" || choice == "/")
+            {
+                result = numOne / numTwo;
+            }
+            
             // Create a simple calculator that asks the user to input two different numbers 
             // and whether they want to add, subtract, multiply or divide them. 
 
